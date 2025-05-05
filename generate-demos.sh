722a29bc1161307b263ad13068f804cd0fdd35b9
Alexander Hansen <alexanderbh@gmail.com>
2025-05-05 12:05:07 +0200

    do not generate if require sudo

--- generate-demos.sh
+++ generate-demos.sh
@@ -3,8 +3,13 @@
 for dir in examples/*/; do
   if [ -d "$dir" ]; then
     if [ -f "${dir}demo.tape" ]; then
-      echo "Running demo in ${dir}"
-      (cd "$dir" && vhs demo.tape)
+      # Check if demo.tape contains "sudo"
+      if grep -q "sudo" "${dir}demo.tape"; then
+        echo "Skipping demo in ${dir} because it contains 'sudo'."
+      else
+        echo "Running demo in ${dir}"
+        (cd "$dir" && vhs demo.tape)
+      fi
     else
       echo "No demo.tape found in ${dir}"
     fi
